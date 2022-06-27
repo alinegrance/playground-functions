d833bbbe1c8f75b129da928236cf64c67680595c
Aline Grance <grance.aline@gmail.com>
2022-06-27 17:47:35 -0300

    Add requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,37 @@
+/* eslint-disable complexity */
+/* eslint-disable max-len */
+/* eslint-disable max-lines-per-function */
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  // create obj with num frequencies
+  const frequencies = {};
+  for (let number of numbers) {
+    if (number in frequencies) {
+      frequencies[number] += 1;
+    } else {
+      frequencies[number] = 1;
+    }
+  }
+
+  // test for length
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  // test if any number shows more than 3 times
+  for (let key in frequencies) {
+    if (frequencies[key] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  // test for < 0 or > 9
+  for (let number of numbers) {
+    if (number < 0 || number > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
 
 // Desafio 12
@@ -8,7 +39,7 @@ function triangleCheck() {
   // seu código aqui
 }
 
-// Desafio 13
+// Desafio 13mas
 function hydrate() {
   // seu código aqui
 }
